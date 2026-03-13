ced9cadc1d543254e0de5acf95c3c61bae9576a1
Malevaniy Anton <goodrazer111@gmail.com>
2026-03-13 19:20:56 +0400

    TaskHard_1 passed.

---
+++ src/HomeWork_1/TaskHard_1.java
@@ -0,0 +1,13 @@
+package HomeWork_1;
+
+public class TaskHard_1 { public static void main(String[] args) {
+    int a, b, sout;
+    a = 1;
+    b = 2;
+    System.out.println("А=" + a + " и " + "B=" + b);
+    sout = a;
+    a = b;
+    b =  sout;
+    System.out.println("А=" + a + " и " + "B=" + b);
+}
+}
